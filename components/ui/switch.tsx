9a223e8d9ab9e412c49871345b1900610f959bc6
Chima Ilo <collins.chima.ilo@gmail.com>
2024-09-14 06:39:11 +0100

    update theme switch component

--- components/ui/switch.tsx
+++ components/ui/switch.tsx
@@ -1,9 +1,10 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import * as SwitchPrimitives from "@radix-ui/react-switch"
+import * as React from "react";
+import * as SwitchPrimitives from "@radix-ui/react-switch";
+import { Moon, Sun } from "lucide-react";
 
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
 
 const Switch = React.forwardRef<
   React.ElementRef<typeof SwitchPrimitives.Root>,
@@ -11,19 +12,32 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={cn(
-      "peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      "peer inline-flex h-8 w-14 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 bg-switch relative",
       className
     )}
     {...props}
     ref={ref}
   >
+    <span className="dark:scale-100 scale-0 transition-transform absolute top-1/2 left-1 text-switch-foreground -translate-y-1/2">
+      <Sun className="w-4 h-4"></Sun>
+    </span>
     <SwitchPrimitives.Thumb
       className={cn(
-        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+        "pointer-events-none block h-7 w-7 relative rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-6 data-[state=unchecked]:translate-x-0"
       )}
-    />
+    >
+      <span className="dark:scale-0 scale-100 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform text-primary">
+        <Sun className="w-4 h-4"></Sun>
+      </span>
+      <span className="dark:scale-100 scale-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 transition-transform text-primary/60">
+        <Moon className="w-4 h-4"></Moon>
+      </span>
+    </SwitchPrimitives.Thumb>
+    <span className="dark:scale-0 scale-100 absolute top-1/2 right-1 text-switch-foreground -translate-y-1/2 transition-transform">
+      <Moon className="w-4 h-4"></Moon>
+    </span>
   </SwitchPrimitives.Root>
-))
-Switch.displayName = SwitchPrimitives.Root.displayName
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
 
-export { Switch }
+export { Switch };
